fc0312d4d8fd4a0dc041e6c6c29b92aef7668e3e
naddi96 <nardidamiano@gmail.com>
2022-03-01 20:04:31 +0100

    modificato link

--- frontend/degree-block/src/App.js
+++ frontend/degree-block/src/App.js
@@ -225,7 +225,7 @@ class App extends React.Component {
         </div>
         <h2>Ops!! Sembra che tu non abbia un wallet</h2>
         <p>Installa un wallet come Metamask e <b>Ricordati di collegare il tuo account al sito!!</b></p>
-        <a href="/">Try again :)</a>
+        <a href="./">Try again :)</a>
         </div>
         </div>
 
